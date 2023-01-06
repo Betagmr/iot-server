f81e4c4f4c8044c48423706cae5abc26eec519aa
Asier Villar <asiervillar@outlook.es>
2023-01-06 16:44:08 +0100

    ADD: POST activator update

--- src/pages/api/raspberry/[id].ts
+++ src/pages/api/raspberry/[id].ts
@@ -3,16 +3,31 @@ import { prisma } from "../../../server/db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
-  const data = await prisma.raspberry.findUnique({
-    where: {
-      id: id as string,
-    },
-  });
 
-  if (data) {
+  if (req.method === "POST") {
+    const { activator } = req.body;
+    const data = await prisma.raspberry.update({
+      where: {
+        id: id as string,
+      },
+      data: {
+        activator,
+      },
+    });
+
     res.status(200).json(data);
-  } else {
-    res.status(404).json({ message: "No data found" });
-  }
 
+  } else if (req.method === "GET") {
+    const data = await prisma.raspberry.findUnique({
+      where: {
+        id: id as string,
+      },
+    });
+
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ message: "No data found" });
+    }
+  }
 } 
