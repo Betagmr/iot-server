cae24cc49e8a28e93eec535b892f22603dbb6ae1
Asier Villar <asiervillar@outlook.es>
2023-01-06 16:44:28 +0100

    ADD: Raspi service

--- src/services/raspiservice.ts
+++ src/services/raspiservice.ts
@@ -10,3 +10,11 @@ export const fetchRaspExist = async (id: string) => {
   const { data } = await axios.get<RaspiRespose[]>("/api/raspberry/" + id);
   return data;
 };
+
+
+export const modifyRaspStatus = async (id: string, status: boolean) => {
+  const { data } = await axios.post<RaspiRespose>("/api/raspberry/" + id, {
+    activator: status,
+  });
+  return data;
+}
